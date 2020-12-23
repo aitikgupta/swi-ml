5e59b0ae2f973c1e49d81ae0eb69c9b1dba01517
Aitik Gupta <aitikgupta@gmail.com>
2020-12-23 05:53:55 +0530

    Added test for Lasso, Ridge, ElasticNet, black compliance

--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -1,9 +1,13 @@
 import pytest
-import numpy as np
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 
-from cu_ml import LinearRegressionGD
+from cu_ml import (
+    LinearRegressionGD,
+    LassoRegressionGD,
+    RidgeRegressionGD,
+    ElasticNetRegressionGD,
+)
 
 X, Y = make_regression(n_samples=1000, n_features=5, random_state=42)
 
@@ -15,10 +19,16 @@ X_train, X_test, Y_train, Y_test = train_test_split(
 def test_linear_regression():
     num_iters = 10
     model_np = LinearRegressionGD(
-        num_iterations=num_iters, learning_rate=0.05, backend="numpy", verbose="ERROR"
+        num_iterations=num_iters,
+        learning_rate=0.05,
+        backend="numpy",
+        verbose="ERROR",
     )
     model_cp = LinearRegressionGD(
-        num_iterations=num_iters, learning_rate=0.05, backend="cupy", verbose="CRITICAL"
+        num_iterations=num_iters,
+        learning_rate=0.05,
+        backend="cupy",
+        verbose="CRITICAL",
     )
 
     with pytest.raises(ValueError):
@@ -31,4 +41,119 @@ def test_linear_regression():
     Y_pred_cp = model_cp.predict(X_test)
 
     assert len(model_np.history) == len(model_cp.history) == num_iters
-    assert Y_pred_np.shape == Y_pred_cp.shape == (34,)
+    assert Y_pred_np.shape == Y_pred_cp.shape
+
+
+def test_lasso_regression():
+    num_iters = 10
+
+    # Needs l1_cost
+    with pytest.raises(TypeError):
+        _ = LassoRegressionGD(
+            num_iterations=num_iters,
+            learning_rate=0.05,
+            backend="numpy",
+            verbose="ERROR",
+        )
+
+    model = LassoRegressionGD(
+        num_iterations=num_iters,
+        learning_rate=0.05,
+        l1_cost=100,
+        backend="numpy",
+        verbose="ERROR",
+    )
+
+    model.fit(X_train, Y_train)
+
+    _ = model.predict(X_test)
+
+
+def test_ridge_regression():
+    num_iters = 10
+
+    # Needs l2_cost
+    with pytest.raises(TypeError):
+        _ = RidgeRegressionGD(
+            num_iterations=num_iters,
+            learning_rate=0.05,
+            backend="numpy",
+            verbose="ERROR",
+        )
+
+    model = RidgeRegressionGD(
+        num_iterations=num_iters,
+        learning_rate=0.05,
+        l2_cost=100,
+        backend="numpy",
+        verbose="ERROR",
+    )
+
+    model.fit(X_train, Y_train)
+
+    _ = model.predict(X_test)
+
+
+def test_elasticnet_regression():
+    num_iters = 10
+
+    # Needs multiplying factor, as well as l1_ratio
+    with pytest.raises(TypeError):
+        _ = ElasticNetRegressionGD(
+            num_iterations=num_iters,
+            learning_rate=0.05,
+            backend="numpy",
+            verbose="ERROR",
+        )
+
+    # Test l1_ratio, initialiser set to zero to have same initial weights
+    elastic_net_lasso_model = ElasticNetRegressionGD(
+        num_iterations=num_iters,
+        learning_rate=0.05,
+        multiply_factor=100,
+        l1_ratio=1,
+        backend="numpy",
+        verbose="ERROR",
+        initialiser="zeros",
+    )
+    elastic_net_ridge_model = ElasticNetRegressionGD(
+        num_iterations=num_iters,
+        learning_rate=0.05,
+        multiply_factor=100,
+        l1_ratio=0,
+        backend="numpy",
+        verbose="ERROR",
+        initialiser="zeros",
+    )
+
+    lasso_model = LassoRegressionGD(
+        num_iterations=num_iters,
+        learning_rate=0.05,
+        l1_cost=100,
+        backend="numpy",
+        verbose="ERROR",
+        initialiser="zeros",
+    )
+    ridge_model = RidgeRegressionGD(
+        num_iterations=num_iters,
+        learning_rate=0.05,
+        l2_cost=100,
+        backend="numpy",
+        verbose="ERROR",
+        initialiser="zeros",
+    )
+
+    elastic_net_lasso_model.fit(X_train, Y_train)
+    elastic_net_lasso_pred = elastic_net_lasso_model.predict(X_test)
+
+    elastic_net_ridge_model.fit(X_train, Y_train)
+    elastic_net_ridge_pred = elastic_net_ridge_model.predict(X_test)
+
+    lasso_model.fit(X_train, Y_train)
+    lasso_pred = lasso_model.predict(X_test)
+
+    ridge_model.fit(X_train, Y_train)
+    ridge_pred = ridge_model.predict(X_test)
+
+    assert (elastic_net_lasso_pred == lasso_pred).all()
+    assert (elastic_net_ridge_pred == ridge_pred).all()
