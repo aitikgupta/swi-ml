75517a7f8ab48c4d52d8c1142b7634cf55fa654f
Aitik Gupta <aitikgupta@gmail.com>
2021-01-24 10:00:28 +0530

    DOC: Finish description and library migration

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cu-ml",
+    name="swi-ml",
     version="0.0.1",
     author="Aitik Gupta",
     author_email="aitikgupta@gmail.com",
-    description="A lighweight Machine Learning library which can run on GPU "
-    "with switchable backends from 'NumPy' to 'CuPy' "
-    "- built from scratch",
+    description="A machine learning library written from scratch "
+    "- with runtime switchable backend!",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/aitikgupta/cu-ml",
+    url="https://github.com/aitikgupta/swi-ml",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
